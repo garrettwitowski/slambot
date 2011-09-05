97abc13a6a118f2c253b52e9ea53da47c8916321
tim.ousley@gmail.com <tim.ousley@gmail.com@16223667-90e6-b9d7-cfa5-ad4b88817f71>
2011-09-05 17:45:04 +0000

    Splitting SlambotCore into it's own dll so that it is testable from NUnit GUI tester

---
+++ source/SlambotCore/NilRGBImageSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Slambot
+{
+    public class NilRGBImageSource: IRGBDImageSource
+    {
+        /// <summary>
+        /// Minimum allowed frame interval
+        /// </summary>
+        protected Double interval;
+
+        /// <summary>
+        /// Stores callback list
+        /// </summary>
+        protected List<RGBDCallback> cbList;
+
+        public NilRGBImageSource()
+        {
+            interval = 0.0;
+            cbList = new List<RGBDCallback>();
+        }
+
+        public void SetFrameInterval(Double seconds)
+        {
+            interval = seconds;
+        }
+
+        public void RegisterRGBDCallback(RGBDCallback cb) 
+        {
+            cbList.Add(cb);
+        }
+
+    }
+}
