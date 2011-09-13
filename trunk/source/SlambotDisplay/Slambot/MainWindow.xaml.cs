8f9af01d1b00cf21f5e6e66038fe7bcf9ab30dc0
garrett.witowski@gmail.com <garrett.witowski@gmail.com@16223667-90e6-b9d7-cfa5-ad4b88817f71>
2011-09-13 23:47:32 +0000

    Dynamically load images into the xaml gui. Added some images to the project so they can be loaded into the gui. Having some issues with the depth images. Throws OutOfMemoryException when depth images are used.

--- trunk/source/SlambotDisplay/Slambot/MainWindow.xaml.cs
+++ trunk/source/SlambotDisplay/Slambot/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace Slambot
 {
@@ -19,10 +21,38 @@ namespace Slambot
     /// </summary>
     public partial class MainWindow : Window
     {
+        int whichImageNumber = 0;
+
         public MainWindow()
         {
             InitializeComponent();
 
         }
-    }
-}
+
+        protected void ConvertToWPF(System.Drawing.Image gdilmg, System.Windows.Controls.Image targetImage)
+        {
+            //convert System.Drawing.Image to WPF Image
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(gdilmg);
+            IntPtr hBitmap = bmp.GetHbitmap();
+            System.Windows.Media.ImageSource WpfBitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            targetImage.Source = WpfBitmap;
+            targetImage.Width = RGBImage.Width;
+            targetImage.Height = RGBImage.Height;
+            targetImage.Stretch = RGBImage.Stretch;
+        } // end of convertToWpf
+
+        public void onClick(object sender, RoutedEventArgs e) 
+        {
+            if (whichImageNumber < 11 && !(RGBImage.Equals(null)) && !(DepthImage.Equals(null)))
+            {
+                whichImageNumber += 5;
+                //FFV: This needs to be implemented later to use the iterator instead of hardcoding it for the individual images
+                System.Drawing.Image img = System.Drawing.Image.FromFile("Data\\image700.tiff");
+                ConvertToWPF(img, RGBImage);
+                System.Drawing.Image depth = System.Drawing.Image.FromFile("Data\\image700.tiff");
+                ConvertToWPF(depth, DepthImage);
+            } //end of if
+        } //end of onClick()
+
+    } //end of MainWindow
+} //end of program
