206e9a255e4a8bee303be89dbc9e5395c2eae14d
tim.ousley@gmail.com <tim.ousley@gmail.com@16223667-90e6-b9d7-cfa5-ad4b88817f71>
2011-09-29 23:36:46 +0000

    Adding files back in that I forgot yesterday

---
+++ source/SlambotTest/DisplayTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit;
+
+using Slambot;
+using System.Drawing;
+
+namespace SlambotTest
+{
+    //Actual Tests
+    [TestFixture]
+    class DisplayTest
+    {
+        //Test functions
+        [Test]
+        public void ConstructorWorks()
+        {
+            //This is the typical setup for a Slambot system:
+            //  a series of constructors chained together around the callback manager
+            var src = new RGBDSourceTestStub();
+            var fs = new FrameStoreBase();
+            var cbm = new CallbackManager(src, fs);
+            var lm = new LandmarkIdentifierBase(cbm);
+            var display = new DisplayBase(cbm);
+            Assert.That(display, Is.Not.Null);
+        }
+
+        //There isn't much more to test because there isn't much more this class does
+    }
+}
---
+++ source/SlambotTest/LandmarkTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit;
+
+using Slambot;
+using System.Drawing;
+
+namespace SlambotTest
+{
+    //Actual Tests
+    [TestFixture]
+    class LandmarkTest
+    {
+        //Test functions
+        [Test]
+        public void ConstructorWorks()
+        {
+            var src = new RGBDSourceTestStub();
+            var fs = new FrameStoreBase();
+            var cbm = new CallbackManager(src, fs);
+            var lm = new LandmarkIdentifierBase(cbm);
+            Assert.That(lm, Is.Not.Null);
+        }
+
+        [Test]
+        public void FindLandmarkListKeyInFrameStore()
+        {
+            var src = new RGBDSourceTestStub();
+            var fs = new FrameStoreBase();
+            var cbm = new CallbackManager(src, fs);
+            var lm = new LandmarkIdentifierBase(cbm);
+            //Pump one image through, then verify that we got some landmarks and the landmark list for that image
+            UInt64 lastId;
+            lastId = src.PumpNewRGBD(Util.GetImage(0), Util.GetDepth(0));
+            //Is Landmark list valid?
+            var llkString = (List<String>)fs.GetConfig()["Landmarks"];
+            Assert.That(llkString, Is.Not.Empty);
+            Assert.That(llkString[0], Is.StringMatching("BaseLandmarks"));
+            //Are there actual landmarks?
+            var landmarks = (List<Landmark>)fs.GetAttributes(lastId)[llkString[0]];
+            Assert.That(landmarks, Is.Not.Empty);
+        }
+
+    }
+}
