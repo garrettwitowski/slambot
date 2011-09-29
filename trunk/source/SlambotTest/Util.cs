a808d77e8b7741eae052287ef356028dfab0c54b
garrett.witowski@gmail.com <garrett.witowski@gmail.com@16223667-90e6-b9d7-cfa5-ad4b88817f71>
2011-09-29 00:54:21 +0000

    Decided to use .png files for all images instead of .tiff files. This has solved the out of memory error when attempting to read an image from a file, specifically with regards to the depth images. There is a known issue with GDI on some versions of windows when reading 16bit gray scale tiffs.

--- trunk/source/SlambotTest/Util.cs
+++ trunk/source/SlambotTest/Util.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Navigation;
 
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace SlambotTest
 {
@@ -24,18 +25,15 @@ namespace SlambotTest
         /// <returns>RGB Image</returns>
         static public Image GetImage(int i)
         {
-
-#if CODEINWORK
-            Stream imageStreamSource = new FileStream("Data\\image"+i+".tiff", FileMode.Open, FileAccess.Read, FileShare.Read);
+            /*Stream imageStreamSource = new FileStream("Data\\image"+i+".tiff", FileMode.Open, FileAccess.Read, FileShare.Read);
             TiffBitmapDecoder decoder = new TiffBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
             BitmapSource bitmapSource = decoder.Frames[0];
-            Bitmap bmp = new Bitmap(bitmapSource.PixelWidth,bitmapSource.PixelHeight,PixelFormat.Format32bppPArgb);
-            BitmapData data = bmp.LockBits(new Rectangle(Point.Empty, bmp.Size),ImageLockMode.WriteOnly,PixelFormat.Format32bppPArgb);
-            source.CopyPixels(Int32Rect.Empty,data.Scan0,data.Height * data.Stride,data.Stride);
+            Bitmap bmp = new Bitmap(bitmapSource.PixelWidth,bitmapSource.PixelHeight,System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            BitmapData data = bmp.LockBits(new Rectangle(System.Drawing.Point.Empty, bmp.Size),ImageLockMode.WriteOnly,System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            bitmapSource.CopyPixels(Int32Rect.Empty,data.Scan0,data.Height * data.Stride,data.Stride);
             bmp.UnlockBits(data);
-            return bmp;
-#endif
-            return Image.FromFile("Data\\image"+i+".tiff"); 
+            return bmp;*/
+            return Image.FromFile("Data\\image"+i+".png"); 
         }
 
         /// <summary>
@@ -45,7 +43,18 @@ namespace SlambotTest
         /// <returns>Depth Image</returns>
         static public Image GetDepth(int i)
         {
-            return Image.FromFile("Data\\depth" + i + ".tiff");
+            /*Stream imageStreamSource = new FileStream("Data\\image" + i + ".tiff", FileMode.Open, FileAccess.Read, FileShare.Read);
+            TiffBitmapDecoder decoder = new TiffBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            BitmapSource bitmapSource = decoder.Frames[0];
+            System.Drawing.Bitmap btm = null;
+            int width = bitmapSource.PixelWidth;
+            int height = bitmapSource.PixelHeight;
+            int stride = width * ((bitmapSource.Format.BitsPerPixel + 7) / 8);
+            IntPtr ptr = System.Runtime.InteropServices.Marshal.AllocHGlobal(height * stride);
+            bitmapSource.CopyPixels(new Int32Rect(0, 0, width, height), ptr, height * stride, stride);
+            btm = new System.Drawing.Bitmap(width, height, stride, System.Drawing.Imaging.PixelFormat.Format1bppIndexed, ptr);
+            return btm;*/
+            return Image.FromFile("Data\\depth" + i + ".png");
         }
     }
 }
